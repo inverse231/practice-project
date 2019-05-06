a143d0eca239cbef3b937b30c8e6757be8795673
Station <3xinverse@gmail.com>
2019-05-06 14:21:29 +0300

    reworked file writing

--- info.php
+++ info.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Form</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css">
     <script src="main.js"></script>
@@ -12,13 +12,26 @@
    
     <?php 
      
-    $hierarchy = $gender = $birthdate = $birthplace = $marriage = $nationality = $degree = $doctorDeg = $language = $nativeLanguage = $surname = $name = $email = "";
+    function print_b($variable, $text){
+        echo "<p1>";
+        print_r($variable);
+        echo "</p1>";
+        echo "<pre>";
+        print_r($variable);
+        echo "</pre>";
+    }
+
+    $hierarchy = $gender = $birthdate = $year = $month = $day = $birthplace = $marriage = $nationality = $degree = $doctorDeg = $language = $nativeLanguage = $surname = $name = $email = "";
     $nameErr = $emailErr = $surnameErr = $birthplaceErr =  "";
     $signed = $selected = "";
 
     //Form input validation
     if($_SERVER["REQUEST_METHOD"] == "POST")
     {
+        $education = $_POST['education'];
+        $doctorDeg = $_POST['doctorDeg'];
+
+
         if(empty($_POST["name"]))
             $nameErr = "Name is required";
         else
@@ -85,8 +98,10 @@
         <input type="radio" name="gender" value="male">Male<br>
         <br>
 
-        Birth date:
-        <input type="text" name="birthdate">
+        Birth date: <br>
+        Day   <input type="text" name="day"><br>
+        Month <input type="text" name="month"><br>
+        Year  <input type="text" name="year"><br>
         <br><br>
 
         Birthplace:
@@ -114,10 +129,15 @@
         
         <br>
 
-		Do you have doctor degree?<br>
-		<input type="radio" name="doctorDeg" value="yes">Yes<br>
-		<input type="radio" name="doctorDeg" value="no">No<br>
+		Do you have PhD or Ph.D?<br>
+		<input type="radio" name="doctorDeg" value="phd">PhD<br>
+		<input type="radio" name="doctorDeg" value="ph.d">Ph.D<br>
+        <input type="radio" name="doctorDeg" balue="no">No<br>
 		<br>
+
+        Name the languages that you speak:<br>
+        <input type="text" name="language">
+        <br><br>
 
         E-mail: <input type ="text" name="email">
         <span class="error">* <?php echo $emailErr; ?> </span>
@@ -132,20 +152,19 @@
     //Writing to file
     $signed = "Signed: ".date("D/M/Y");
     $birthdate = strtotime($birthdate);
-    $myfile = fopen("newfile.txt", "w") or die ("Unable to open file");
+    //$myfile = fopen("newfile.txt", "w") or die ("Unable to open file");
     if(isset($_POST['submit']))
     {
         $selected = $_POST['marriage'];
     }
 
+    print_b($education,"TEST");
 
-    fwrite($myfile, $name.PHP_EOL);
-    fwrite($myfile, $surname.PHP_EOL);
-    fwrite($myfile, $birthdate.PHP_EOL);
-    fwrite($myfile, $birthplace.PHP_EOL);
-    fwrite($myfile, $email.PHP_EOL);
-    fwrite($myfile, $signed.PHP_EOL);
-    fclose($myfile);
+    file_put_contents("newfile.txt", '');
+    file_put_contents("newfile.txt", $name.PHP_EOL, FILE_APPEND);       //change fwrite to fileputcontents
+    file_put_contents("newfile.txt", $surname.PHP_EOL, FILE_APPEND);
+    file_put_contents("newfile.txt", $email.PHP_EOL, FILE_APPEND);
+    file_put_contents("newfile.txt", $birthdate.PHP_EOL, FILE_APPEND);
 
     ?>    
 
@@ -153,17 +172,12 @@
     <?php
     //Test variable output
     echo "<h2>Your input:</h2>";
-    echo "$name <br>";
-    echo "$surname <br>";
-    echo "$email <br>";
-    echo "$gender <br>";
-    echo "$birthdate <br>";
-    echo "$birthplace <br>";
-    echo "$selected <br>";
-    echo $signed;
+    print_b($name, "Test");
+    print_b($surname, "Test");
+    print_b($email, "Test");
+    //print_b($gender, "Test");
+    print_b($birthdate, "Test");
     ?>
 
-
-    
 </body>
 </html>
